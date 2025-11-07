e1e1f3856ed0b724acfe30b7252e77617f277dbd
FeusX <feusuaigax@proton.me>
2025-11-07 17:28:25 +0300

    something

---
+++ arduino-nano-interpreter.ino
@@ -0,0 +1,86 @@
+#include "cmd.h"
+
+void handleCmd(char *input)
+{
+  int len = strlen(input);
+  while (len > 0 && (input[len - 1] == '\r' || input[len - 1] == '\n'))
+    input[--len] = '\0';
+
+  while (*input == ' ') input++;
+
+  char *name = strtok(input, "(");
+  char *arg_str = strtok(NULL, ")");
+  char *args[8];
+  int argc = 0;
+
+  if (arg_str != NULL)
+  {
+    char clean_args[64];
+    int j = 0;
+    for (int i = 0; arg_str[i] != '\0'; i++)
+      if (arg_str[i] != ' ' && arg_str[i] != '\r' && arg_str[i] != '\n')
+        clean_args[j++] = arg_str[i];
+    clean_args[j] = '\0';
+
+    // Tokenize by comma
+    char *token = strtok(clean_args, ",");
+    while (token != NULL && argc < 8)
+    {
+      args[argc++] = token;
+      token = strtok(NULL, ",");
+    }
+  }
+
+  // debugger
+  /*Serial.print("Parsed command name: ");
+  Serial.println(name);
+  Serial.print("Arg count: ");
+  Serial.println(argc);
+  for (int j = 0; j < argc; j++) {
+    Serial.print("Arg[");
+    Serial.print(j);
+    Serial.print("]: ");
+    Serial.println(args[j]);
+  }*/
+
+  // Run matching command
+  for (int i = 0; i < command_num; i++)
+  {
+    if (strcasecmp(name, commands[i].name) == 0)
+    {
+      commands[i].func(args, argc);
+      return;
+    }
+  }
+
+  Serial.print("[ERROR] Unknown command: ");
+  Serial.println(name);
+}
+
+
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println("ready");
+  delay(200);
+}
+
+void loop()
+{
+  static char input_buffer[64];
+  static int index = 0;
+
+  while(Serial.available())
+  {
+    char ch = Serial.read();
+
+    if(ch == '\n' || ch == '\r')
+    {
+      input_buffer[index] = '\0';
+      if(index > 0) handleCmd(input_buffer);
+      index = 0;
+    }
+
+    else if(index < sizeof(input_buffer) - 1) {input_buffer[index++] = ch;}
+  }
+}
--- cmd.h
+++ cmd.h
@@ -1,41 +1,72 @@
 #ifndef CMD_H
 #define CMD_H
 
-typedef void (*CmdFunc) (char *args);
+#include <Arduino.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef void (*CmdFunc) (char **args, int argc);
 
 typedef struct {
   const char *name;
   CmdFunc func;
 } Command;
 
+void writeLed(char **args, int argc);
+//void help(char **args, int argc);
+//void getInput(char **args, int argc);
+//void scriptCmd(char **args, int argc);
+void echoCmd(char **args, int argc);
+void sleepCmd(char **args, int argc);
+
 const Command commands[] = {
-  { "WRITE LED", writeLed },
-  { "WRITE CMD", writeCmd},
-  { "HELP", help},
-  { "INPUT", getInput}
+  { "writeLed", writeLed },
+  { "echoCmd", echoCmd},
+  /*{ "HELP", help},
+  { "INPUT", getInput},
+  { "SCRIPT", scriptCmd},*/
+  { "sleepCmd", sleepCmd}
 };
 
 const int command_num = sizeof(commands) / sizeof(commands[0]);
 
-int writeLed(char **args, int argc) {
-  if(argc < 2) Serial.println("[ERROR] Usage: writeLed(pin, value)"); return;
+void writeLed(char **args, int argc)
+{
+  delay(10);
+  if(argc < 2)
+  {
+    Serial.println("[ERROR] Usage: writeLed(pin, value)"); 
+    return;
+  }
 
   int8_t pin = atoi(args[0]);
   int8_t val = atoi(args[1]);
 
   pinMode(pin, OUTPUT);
 
-  if(val > 255) Serial.println("[ERROR] value can't be greater than 255");
+  if(val > 255)
+  {
+    Serial.println("[ERROR] Value can't be greater than 255");
+    return;
+  }
 
   if(val > 1) analogWrite(pin, val);
   else digitalWrite(pin, val ? HIGH : LOW);
 
-  return;
+  Serial.println("ledwritten");
 }
 
-void writeCmd(char **args, int argc) {
-  if(argc < 1) Serial.println(" ");
-  // echo
+void echoCmd(char **args, int argc)
+{
+  if(argc < 1) Serial.println("[ERROR] Usage: echoCmd(msg)"); return;
+  Serial.println(args[0]);
+}
+
+void sleepCmd(char **args, int argc)
+{
+  if(argc < 1) Serial.println("nope"); return;
+
+  delay(atoi(args[0]));
 }
 
 #endif
